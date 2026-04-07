01ca93cd468baeacd945a6d05425c59cd2906150
Leonids <A231086LK@rvt.lv>
2026-04-07 20:32:34 +0300

    update database seeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Game;
-use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -11,21 +9,9 @@ class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         $this->call(StoreSeeder::class);
         $this->call(GameSeeder::class);
-
-        User::query()->firstOrCreate(
-            ['email' => 'test@example.com'],
-            [
-                'nickname' => 'Test User',
-                'password' => 'password',
-                'role' => 'user',
-            ],
-        );
     }
 }
